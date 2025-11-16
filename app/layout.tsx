882c12f0ce8bbc72a511393692c95e353afc88ba
poyu <poyuwork06115@gmail.com>
2025-11-16 15:08:41 +0800

    fix: 修正toast無法顯示顏色問題

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <Providers>
           {children}
-          <Toaster position="top-center" />
+          <Toaster position="top-center" richColors />
         </Providers>
       </body>
     </html>
